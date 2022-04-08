eb45f45691a0a81b4278a53c9cd4e3b6cab8f2ed
McManOfTheLand <forrestchew@gmail.com>
2022-04-08 16:54:43 -0700

    changed some names and set gas limit to some above 200k

--- src/components/ListNftLoggedIn.js
+++ src/components/ListNftLoggedIn.js
@@ -4,21 +4,25 @@ import contractAddress from '../nft-auction-contract-info/contract-address';
 import abi from '../nft-auction-contract-info/abi';
 
 const ListNftLoggedIn = () => {
+  // Calls the listNftForAuction function within smart contract on form submission.
+  // Auction owner fee will automatically be applied, and is reflected in MetaMask.
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const funcCallOptions = {
-      value: ethers.utils.parseEther('0.01'),
-      gasLimit: 30000,
-    };
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contractInstance = new ethers.Contract(contractAddress, abi, signer);
-    await contractInstance.listNftForAuction(
+    let txOptions = {
+      value: ethers.utils.parseEther('0.01'),
+      gasLimit: 325000,
+    };
+    const txResponse = await contractInstance.listNftForAuction(
       listNftForm.tokenFactoryAddr,
       listNftForm.tokenId,
-      listNftForm.startingPrice,
-      funcCallOptions
+      ethers.utils.parseEther(listNftForm.startingPrice),
+      txOptions
     );
+    const txReceipt = await txResponse.wait();
+    console.log(txReceipt);
   };
 
   const [listNftForm, setListNftForm] = useState({
@@ -56,24 +60,22 @@ const ListNftLoggedIn = () => {
             <label className='block text-center mt-12'>
               <p className='text-2xl'>NFT ID</p>
               <input
-                className='text-black text-lg mt-2 border-2 rounded-md placeholder:text-sm'
-                type='text'
+                className='text-black text-lg w-full mt-2 border-2 rounded-md placeholder:text-sm'
+                type='number'
                 name='tokenId'
                 value={listNftForm.tokenId}
                 onChange={handleFormInputChange}
-                size='30'
                 required
               />
             </label>
             <label className='block text-center mt-12'>
-              <p className='text-2xl'>Starting Price:</p>
+              <p className='text-2xl'>Starting Price</p>
               <input
-                className='text-black text-lg mt-2 border-2 rounded-md placeholder:text-sm'
-                type='text'
+                className='text-black text-lg w-full mt-2 border-2 rounded-md placeholder:text-sm'
+                type='number'
                 name='startingPrice'
                 value={listNftForm.startingPrice}
                 onChange={handleFormInputChange}
-                size='30'
                 required
               />
             </label>
