cd9a7a11f81c70ba710cec708b17d84fc13510a0
McManOfTheLand <forrestchew@gmail.com>
2022-04-19 17:03:09 -0700

    Create instance of active auction NFT in Moralis DB

--- src/components/ListNftLoggedIn.js
+++ src/components/ListNftLoggedIn.js
@@ -1,95 +1,138 @@
 import { useState } from 'react';
 import { ethers } from 'ethers';
+import { useNewMoralisObject, useMoralisQuery } from 'react-moralis';
 import contractAddress from '../nft-auction-contract-info/contract-address';
 import abi from '../nft-auction-contract-info/abi';
+import '../styles/list-nft-container.css';
 
 const ListNftLoggedIn = () => {
-  // Calls the listNftForAuction function within smart contract on form submission.
-  // Auction owner fee will automatically be applied, and is reflected in MetaMask.
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const contractInstance = new ethers.Contract(contractAddress, abi, signer);
-    let txOptions = {
-      value: ethers.utils.parseEther('0.01'),
-      gasLimit: 325000,
-    };
-    const txResponse = await contractInstance.listNftForAuction(
-      listNftForm.tokenFactoryAddr,
-      listNftForm.tokenId,
-      ethers.utils.parseEther(listNftForm.startingPrice),
-      txOptions
-    );
-    const txReceipt = await txResponse.wait();
-    console.log(txReceipt);
-  };
-
   const [listNftForm, setListNftForm] = useState({
     tokenFactoryAddr: '',
     tokenId: '',
     startingPrice: '',
+    userAddress: '',
   });
-
-  const handleFormInputChange = (e) => {
-    const inputName = e.target.name;
-    const inputValue = e.target.value;
-    setListNftForm({ ...listNftForm, [inputName]: inputValue });
+  const { save } = useNewMoralisObject('ActiveNft');
+  const createAuctionItemInDB = async (seller, nftFactoryAddr, id, price) => {
+    const data = {
+      NFTSeller: seller,
+      NFTFactoryAddress: nftFactoryAddr,
+      TokenId: id,
+      NFTPrice: price,
+    };
+    save(data, {
+      onSuccess: (activeNft) => {
+        console.log(activeNft);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
   };
 
+  // Calls the listNftForAuction function within smart contract when form is submitted.
+  // Auction owner fee of 0.01 ETH will automatically be applied, and is reflected in MetaMask.
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    createAuctionItemInDB(
+      listNftForm.userAddress,
+      listNftForm.tokenFactoryAddr,
+      listNftForm.tokenId,
+      listNftForm.startingPrice
+    );
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contractInstance = new ethers.Contract(
+        contractAddress,
+        abi,
+        signer
+      );
+      let txOptions = {
+        value: ethers.utils.parseEther('0.01'),
+        gasLimit: 325000,
+      };
+      const txResponse = await contractInstance.listNftForAuction(
+        listNftForm.tokenFactoryAddr,
+        listNftForm.tokenId,
+        ethers.utils.parseEther(listNftForm.startingPrice),
+        txOptions
+      );
+      const txReceipt = await txResponse.wait();
+      console.log(txReceipt);
+    } catch (error) {
+      console.log(error);
+      console.log('Admin may have to delete entry from DB');
+    }
+  };
+
+  const handleFormInputChange = async (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const userAddress = await signer.getAddress();
+    setListNftForm({
+      ...listNftForm,
+      [inputName]: inputValue,
+      userAddress: userAddress,
+    });
+  };
+  const placeHolderInputAddr = '0x0000'.padEnd(20, '.');
   return (
     <>
-      <div className='z-0 flex flex-col justify-center items-center'>
-        <h1 className='text-4xl mt-10 text-center underline'>List NFT</h1>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <label className='block text-center mt-12'>
-              <p className='text-2xl'>NFT Contract Address:</p>
-              <input
-                className='text-black text-lg mt-2 border-2 rounded-md'
-                type='text'
-                name='tokenFactoryAddr'
-                value={listNftForm.tokenFactoryAddr}
-                onChange={handleFormInputChange}
-                size='30'
-                maxLength='42'
-                minLength='42'
-                required
-              />
-            </label>
-            <label className='block text-center mt-12'>
-              <p className='text-2xl'>NFT ID</p>
-              <input
-                className='text-black text-lg w-full mt-2 border-2 rounded-md placeholder:text-sm'
-                type='number'
-                name='tokenId'
-                value={listNftForm.tokenId}
-                onChange={handleFormInputChange}
-                required
-              />
-            </label>
-            <label className='block text-center mt-12'>
-              <p className='text-2xl'>Starting Price</p>
-              <input
-                className='text-black text-lg w-full mt-2 border-2 rounded-md placeholder:text-sm'
-                type='number'
-                name='startingPrice'
-                value={listNftForm.startingPrice}
-                onChange={handleFormInputChange}
-                required
-              />
-            </label>
-            <br></br>
-            <div className='text-center mt-12'>
-              <button
-                className='border-2 rounded-lg mb-10 py-2 px-6 bg-red-500 border-red-500 '
-                type='submit'
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
+      <div className='input-container'>
+        <h1>List NFT</h1>
+        <form onSubmit={handleSubmit} className='form'>
+          <label>
+            <p>NFT Contract Address:</p>
+            <input
+              className='input-box'
+              type='text'
+              name='tokenFactoryAddr'
+              value={listNftForm.tokenFactoryAddr}
+              onChange={handleFormInputChange}
+              placeholder={placeHolderInputAddr}
+              size='30'
+              maxLength='42'
+              minLength='42'
+              required
+            />
+          </label>
+          <label>
+            <p>NFT ID</p>
+            <input
+              className='input-box'
+              type='number'
+              name='tokenId'
+              value={listNftForm.tokenId}
+              onChange={handleFormInputChange}
+              placeholder='NFT ID'
+              min='0'
+              required
+            />
+          </label>
+          <label>
+            <p>Starting Price</p>
+            <input
+              className='input-box'
+              type='number'
+              name='startingPrice'
+              value={listNftForm.startingPrice}
+              onChange={handleFormInputChange}
+              placeholder='0.00 ETH'
+              step='.01'
+              min='0'
+              required
+            />
+          </label>
+          <br></br>
+          <div>
+            <button className='btn' type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
--- src/components/LiveAuction.js
+++ src/components/LiveAuction.js
@@ -1,9 +1,10 @@
 import ReactHlsPlayer from '@panelist/react-hls-player';
+import '../styles/video-player-container.css';
 
 const LiveAuction = () => {
   return (
     <>
-      <div className='basis-10/12 border-2 bg-stone-700 ml-10 mt-10 mb-auto mr-12'>
+      <div className='video-player-container'>
         <ReactHlsPlayer
           //   src='http://192.168.0.18:8080/hls/stream.m3u8'
           autoPlay={false}
@@ -11,9 +12,7 @@ const LiveAuction = () => {
           width='100%'
           height='auto'
         />
-        <p className='text-white text-4xl ml-20 mt-5 pb-5 self-end underline'>
-          Live NFT Auction
-        </p>
+        <p className='video-player-paragraph'>Live NFT Auction</p>
       </div>
     </>
   );
