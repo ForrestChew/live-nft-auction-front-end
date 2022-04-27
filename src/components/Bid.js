a716cc9d780b675c208d8568e5daeb3de02343cf
McManOfTheLand <forrestchew@gmail.com>
2022-04-26 19:18:01 -0700

    Added entire functionality to match the NFT that is auctioning on blockchain with what data is being fetched from the Moralis DB. This was acheived by pulling events emitted from smart contract, logged in DB and pulled from client

--- src/components/Bid.js
+++ src/components/Bid.js
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { ethers } from 'ethers';
+import {
+  useMoralisQuery,
+  useMoralisSubscription,
+  useMoralis,
+} from 'react-moralis';
 import abi from '../nft-auction-contract-info/abi';
 import contractAddress from '../nft-auction-contract-info/contract-address';
 
 const Bid = () => {
-  const [bidAmount, setBidAmount] = useState('0');
+  const [activeNftSeller, setActiveNftSeller] = useState('');
+  const [activeNftFactoryAddress, setActiveNftFactoryAddress] = useState('');
+  const [activeNftPriceTokenId, setActiveNftTokenId] = useState(0);
+  const [activeNftPrice, setActiveNftPrice] = useState(0);
+  const [activeNftHighestBidder, setActiveNftHighestBidder] = useState('');
+  const [bidAmount, setBidAmount] = useState(0);
+
+  const hasFetchedData = useRef(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -22,6 +34,89 @@ const Bid = () => {
     const inputValue = e.target.value;
     setBidAmount(inputValue);
   };
+
+  // Emitted on NFT sale.
+  useMoralisSubscription(
+    'NftSale',
+    (query) => query.ascending('createdAt').limit(1),
+    [],
+    {
+      live: true,
+      onCreate: () => {
+        destroyOldAuctionNft();
+      },
+    }
+  );
+
+  const { Moralis } = useMoralis();
+  const destroyOldAuctionNft = async () => {
+    const ActiveNft = Moralis.Object.extend('NftForAuction');
+    const query = new Moralis.Query(ActiveNft);
+    const activeNftQuery = await query.find();
+    const nftToDestroy = activeNftQuery[0];
+    await nftToDestroy.destroy().then(() => {
+      setAuctionNft();
+    });
+  };
+
+  // Emitted on New bid placed on NFT
+  useMoralisSubscription(
+    'NewBids',
+    (query) => query.descending('createdAt').limit(1),
+    [],
+    {
+      live: true,
+      onCreate: (data) => {
+        // Destructres new bid object to update NFT info with
+        const { bidAmount, bidder } = data.attributes;
+        hasFetchedData.current = false;
+        updateBidderAndPrice(bidAmount, bidder);
+      },
+    }
+  );
+
+  // Called upon Newbid event
+  const updateBidderAndPrice = async (newPrice, highestBidder) => {
+    const newPriceFromWei = ethers.utils.formatEther(newPrice);
+    // Updates Auction NFT price and it's highest bidder in order to server new clients
+    const ActiveNft = Moralis.Object.extend('NftForAuction');
+    const query = new Moralis.Query(ActiveNft);
+    const activeNftQuery = await query.find();
+    const nftToUpdate = activeNftQuery[0];
+    nftToUpdate.set('price', newPrice);
+    nftToUpdate.set('highestBidder', highestBidder);
+    nftToUpdate.save().then(() => {
+      setActiveNftPrice(newPriceFromWei);
+      setActiveNftHighestBidder(highestBidder);
+    });
+  };
+
+  const { fetch } = useMoralisQuery(
+    'NftForAuction',
+    (query) => query.ascending('createdAt').limit(1),
+    []
+  );
+
+  const setAuctionNft = async () => {
+    await fetch().then((nft) => {
+      const { seller, tokenFactoryAddr, tokenId, price, highestBidder } =
+        nft[0].attributes;
+      const formattedPrice = ethers.utils.formatEther(price);
+      setActiveNftSeller(seller);
+      setActiveNftFactoryAddress(tokenFactoryAddr);
+      setActiveNftTokenId(tokenId);
+      setActiveNftPrice(formattedPrice);
+      setActiveNftHighestBidder(highestBidder);
+    });
+  };
+  // Sets initial auction NFT info
+  useEffect(() => {
+    if (!hasFetchedData.current) {
+      setAuctionNft();
+      hasFetchedData.current = true;
+    }
+  }, []);
+
   return (
     <>
       <div
@@ -30,15 +125,15 @@ const Bid = () => {
       >
         <form className='form' onSubmit={handleSubmit}>
           <label>
-            <p>Place Bid</p>
+            <p style={{ textDecoration: 'underline' }}>Place Bid</p>
             <input
               className='input-box'
               type='number'
               value={bidAmount}
               onChange={handleFormInputChange}
-              placeholder='0.00 ETH'
+              placeholder={`${activeNftPrice} ETH`}
               step='.01'
-              min='0'
+              min={activeNftPrice}
               required
             />
           </label>
@@ -46,7 +141,10 @@ const Bid = () => {
           <div>
             <button
               className='btn'
-              style={{ marginBottom: '2.75rem' }}
+              style={{
+                marginBottom: '2rem',
+                padding: '.5rem 1rem .5rem 1rem',
+              }}
               type='submit'
             >
               Submit
@@ -57,36 +155,28 @@ const Bid = () => {
           <table className='table'>
             <thead>
               <tr>
-                <th width='30%'>Current Bid:</th>
-                <th>{'0 ETH'}</th>
+                <th width='30%'>Current Price:</th>
+                <th>{`${activeNftPrice} ETH`}</th>
               </tr>
               <tr>
-                <th>Highest Bidder</th>
-                <th>{'0x'.padEnd(42, '0')}</th>
+                <th>Token ID</th>
+                <th>{activeNftPriceTokenId}</th>
               </tr>
               <tr>
                 <th>NFT Factory address</th>
-                <th>{'0x'.padEnd(42, '0')}</th>
+                <th>{activeNftFactoryAddress}</th>
               </tr>
               <tr>
                 <th>NFT Seller</th>
-                <th>{'0x'.padEnd(42, '0')}</th>
+                <th>{activeNftSeller}</th>
               </tr>
               <tr>
-                <th>To Be Decided</th>
-                <th>{'Some random info'}</th>
-              </tr>
-              <tr>
-                <th>To Be Decided</th>
-                <th>{'Some random info'}</th>
-              </tr>
-              <tr>
-                <th>To Be Decided</th>
-                <th>{'Some random info'}</th>
-              </tr>
-              <tr>
-                <th>To Be Decided</th>
-                <th>{'Some random info'}</th>
+                <th>Highest Bidder</th>
+                <th>
+                  {activeNftHighestBidder === '0x'.padEnd(42, '0')
+                    ? 'No bids yet'
+                    : activeNftHighestBidder}
+                </th>
               </tr>
             </thead>
           </table>
